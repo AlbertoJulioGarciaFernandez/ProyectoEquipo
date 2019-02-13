566199fa4895826bd2394332b5cfc2b05f1e9a77
U_35007842 <U_35007842@PC2>
2019-02-13 11:28:03 +0000

    Primer paso

---
+++ src/proyectoenequipo/CadenaTexto.java
@@ -0,0 +1,10 @@
+package proyectoenequipo;
+/**
+ *
+ * @author Dani y Alberto (ETS)
+ */
+public class CadenaTexto {
+    private String cadena="Proyecto";
+    private String cadena2=" ETS ";
+    private String cadena3="Dani y Alberto";            
+}
