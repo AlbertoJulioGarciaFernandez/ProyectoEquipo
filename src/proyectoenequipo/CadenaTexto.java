c4f6507c3af5dafa89a663a99e0d81af6b15c585
U_35007842 <U_35007842@PC2>
2019-02-13 11:54:15 +0000

    Segundo paso (getters y setters)

--- src/proyectoenequipo/CadenaTexto.java
+++ src/proyectoenequipo/CadenaTexto.java
@@ -10,5 +10,31 @@ public class CadenaTexto {
     *
     */
     private String cadena2=" ETS ";
-    private String cadena3="Dani y Alberto";            
+    private String cadena3="Dani y Alberto"; 
+
+    public String getCadena() {
+        return cadena;
+    }
+
+    public void setCadena(String cadena) {
+        this.cadena = cadena;
+    }
+
+    public String getCadena2() {
+        return cadena2;
+    }
+
+    public void setCadena2(String cadena2) {
+        this.cadena2 = cadena2;
+    }
+
+    public String getCadena3() {
+        return cadena3;
+    }
+
+    public void setCadena3(String cadena3) {
+        this.cadena3 = cadena3;
+    }
+    
+    
 }
